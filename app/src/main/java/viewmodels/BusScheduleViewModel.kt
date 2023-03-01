71053d39670deab3471324ddd444b69bbc94a42e
Ahmed <105488858+baldia275@users.noreply.github.com>
2023-03-01 11:30:14 +0100

    fin du TP

--- app/src/main/java/viewmodels/BusScheduleViewModel.kt
+++ app/src/main/java/viewmodels/BusScheduleViewModel.kt
@@ -4,9 +4,11 @@ package viewmodels
 import androidx.lifecycle.ViewModel
 import com.example.busschedule.database.Schedule
 import com.example.busschedule.database.ScheduleDao
+import kotlinx.coroutines.flow.Flow
 
 class BusScheduleViewModel(private val scheduleDao: ScheduleDao): ViewModel() {
-    fun fullSchedule(): List<Schedule> = scheduleDao.getAll()
 
-    fun scheduleForStopName(name: String): List<Schedule> = scheduleDao.getByStopName(name)
+    fun fullSchedule(): Flow<List<Schedule>> = scheduleDao.getAll()
+
+    fun scheduleForStopName(name: String): Flow<List<Schedule>> = scheduleDao.getByStopName(name)
 }
